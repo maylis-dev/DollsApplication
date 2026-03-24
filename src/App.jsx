9bfd544955f0fb4b4f82a984c78729f9fa0d256b
maylis imperiali <imp.mayli@gmail.com>
2026-03-24 11:21:20 +0100

    newne

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import BarbiePage from "./pages/BarbiePage";
-import ProductDetailPage from "./pages/ProductDetailPage";
 import ProductsPage from "./pages/ProductsPage";
 import Loginpage from "./pages/Loginpage";
 import Signuppage from "./pages/Signuppage";
@@ -14,6 +13,7 @@ import { AuthWrapper } from "./context/auth.context";
 import EditProductsPage from "./pages/EditProduct";
 import BratzPage from "./pages/BratzPage";
 import Private from "./components/Private";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 
 function App() {
